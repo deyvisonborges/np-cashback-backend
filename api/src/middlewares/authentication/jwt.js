ac5ecb8a51626d2fa1544506822ec9a224a859ef
Deyvison Borges <web.dborges@gmail.com>
2020-05-22 17:31:27 -0300

    => feature[middlewares/jwt.js]('adicionando servico de autenticao')

--- api/src/middlewares/authentication/jwt.js
+++ api/src/middlewares/authentication/jwt.js
@@ -1,23 +1,37 @@
 const jwt = require('jsonwebtoken');
-const secret_key = require('../../config/keys/security');
 
-exports.authorize = async (req, res, next) => {
-    try {
-        // const _header = req.headers.authorization;  
-        // console.log(_header)
-        // if(!_header) return res.send('Acesso restrito');
-        // next();
-        // ainda nao implementei por completo ;)
-    } catch (err) {
-        next(err);
+const authorize = (SECRET_AUTH) => (req, res, next) => {
+  try {
+    if (!SECRET_AUTH) {
+      throw new Error('[DEV_ERROR] Você precisa injetar uma config_auth para poder autorizar a requisição!');
+    } else {
+      const header_token = req.headers['authorization'] || req.headers['authorization'] || window.localStorage.getItem('token');
+      if (!header_token)
+        throw new Error('Token not found');
+      return next();  
     }
+  } catch (err) {
+    next(req.body.err_message = err.message);
+  }
 }
 
-exports.generateToken = async (data) => {
-    return await jwt.sign(data, secret_key.auth.secret);
-}
-
-exports.decoded = async (token) => {
-    const data = await jwt.decode(token, Security.AuthJson.secret);
+const generate = (payload, SECRET_AUTH) => err => {
+  if(err) {
+    throw new Error('Não foi possível gerar o token.');
+  } else {
+    const data = jwt.sign(payload, SECRET_AUTH);
     return data;
+  }
+}
+
+const decode = (payload, SECRET_AUTH) => err => {
+  if (err) {
+    throw new Error('Não foi decodificar o token.');
+  }
+  const data = jwt.decode(payload, SECRET_AUTH);
+  return data
+}
+
+module.exports = {
+  authorize, generate, decode
 }
